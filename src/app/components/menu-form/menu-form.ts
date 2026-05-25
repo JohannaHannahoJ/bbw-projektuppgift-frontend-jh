00d268125c3f778bc864d5596194be7f7772ba37
JohannaHannahoJ <joha2102@student.miun.se>
2026-05-25 16:54:24 +0200

    Add menu management with create and delete functionality

--- src/app/components/menu-form/menu-form.ts
+++ src/app/components/menu-form/menu-form.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
+import { MenuService } from '../../core/services/menu.service';
+import { CategoryService } from '../../core/services/category.service';
+import { MenuItem } from '../../core/models/menu-item';
 
 @Component({
   selector: 'app-menu-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './menu-form.html',
   styleUrl: './menu-form.css',
 })
-export class MenuForm {}
+export class MenuForm {
+  name: string = "";
+  price: number = 0;
+  description: string = "";
+  is_available: boolean = true;
+  is_offer: boolean = false;
+  category_id: number = 0;
+
+  message = signal(""); // För meddelanden i frontend
+
+  authService = inject(AuthService);
+  menuService = inject(MenuService);
+  categoryService = inject(CategoryService);
+
+  // kategorier till dropdown
+  categories = this.categoryService.categories;
+
+  addMenuItem(): void {
+
+    const menuItem: MenuItem = {
+      name: this.name,
+      price: this.price,
+      description: this.description,
+      is_available: this.is_available,
+      is_offer: this.is_offer,
+      category_id: this.category_id
+    };
+
+    this.menuService.addMenuItem(menuItem).subscribe({
+
+      next: () => {
+
+        this.message.set("Rätt tillagd");
+
+        // töm formulär
+        this.name = "";
+        this.price = 0;
+        this.description = "";
+        this.is_available = true;
+        this.is_offer = false;
+        this.category_id = 0;
+      },
+
+      error: (error) => {
+        this.message.set(error.error.message);
+      }
+    });
+  }
+}
--- src/app/components/menu-management/menu-management.ts
+++ src/app/components/menu-management/menu-management.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MenuService } from '../../core/services/menu.service';
+import { MenuItem } from '../../core/models/menu-item';
 
 @Component({
   selector: 'app-menu-management',
@@ -6,4 +8,47 @@ import { Component } from '@angular/core';
   templateUrl: './menu-management.html',
   styleUrl: './menu-management.css',
 })
-export class MenuManagement {}
+export class MenuManagement {
+  menuService = inject(MenuService);
+
+  // Hämta signal från service som listar meny-items
+  menuItems = this.menuService.menuItems;
+
+  // kontrollerar vilket item som redigeras
+  selectedItem: MenuItem | null = null;
+
+  ngOnInit() {
+    // läser in items vid sidladdning
+    this.menuService.loadMenuItems();
+  }
+
+  // Regigera meny-item
+  editItem(item: MenuItem) {
+    //sparar valt item
+    this.selectedItem = item;
+    // form kommer
+    console.log("Edit item:", item);
+  }
+
+  // ta bort item
+  deleteItem(id: number) {
+    this.menuService.deleteMenuItem(id).subscribe(() => {
+      // ladda om listan efter delete
+      this.menuService.loadMenuItems();
+    });
+  }
+
+  // Uppdatera 
+  updateMenuItem(updatedItem: MenuItem) {
+    if (!this.selectedItem) return;
+
+    // skickar uppdaterat item till db
+    this.menuService.updateMenuItem(this.selectedItem.id!, updatedItem)
+      .subscribe(() => {
+        // Ladda om lista vid update
+        this.menuService.loadMenuItems();
+        // Rensa redigera
+        this.selectedItem = null;
+      });
+  }
+}
