c71e146ef2e47045e1176d72aa29ef852c090bc7
JohannaHannahoJ <joha2102@student.miun.se>
2026-05-31 14:29:08 +0200

    Add about section and image gallery

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GuestMenu } from '../../components/guest-menu/guest-menu';
 import { MessageForm } from '../../components/message-form/message-form';
+import { About } from "../../components/about/about";
 
 @Component({
   selector: 'app-home',
-  imports: [GuestMenu, MessageForm],
+  imports: [GuestMenu, MessageForm, About],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
